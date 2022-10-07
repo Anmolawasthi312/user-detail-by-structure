999901af5b1fa5bce9c654691e6d1655a6940c59
Anmolawasthi312 <115224767+Anmolawasthi312@users.noreply.github.com>
2022-10-07 16:30:25 +0530

    Add files via upload

---
+++ user detail by structure.cpp
@@ -0,0 +1,65 @@
+#include<stdio.h>
+struct userdetail
+{
+	int age;
+	char name[10];
+	int che, btech , bpharma;
+} pa;
+int main(){
+	int d;
+		pa.name;
+		pa.age;
+		pa.che;
+		pa.btech;
+		pa.bpharma;
+		
+		
+		
+		
+		
+		
+		{
+	printf("enter the marks  of the che: ");
+	scanf("  %d",&pa.che);
+}
+
+{
+	printf("enter the marks  of the btech: ");
+	scanf("  %d",&pa.btech);
+}
+
+
+{
+	printf("enter the marks  of the bharma: ");
+	scanf("  %d",&pa.bpharma);
+}
+		
+		
+		{
+	printf("enter the age of the user : ");
+	scanf("  %d",&pa.age);
+}
+
+
+
+
+
+
+
+		{
+	printf("enter the name of the name : ");
+
+	scanf("  %s",&pa.name);
+	
+}
+
+	
+
+	d=pa.che+pa.btech+pa.bpharma;
+	printf(" total marks is : %d\n ",d);
+printf(" user name is : %s \n",pa.name);
+	printf(" user age is : %d ",pa.age);
+
+}
+
+
